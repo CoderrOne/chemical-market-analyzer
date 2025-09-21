f472a7a5186aad3003f6f5e767a82826294a8df9
CoderrOne <abdirahman1404@outlook.com>
2025-09-21 15:42:25 +0100

    Add project files.

---
+++ chemical-market-analyzer.cpp
@@ -0,0 +1,12 @@
+﻿// chemical-market-analyzer.cpp : Defines the entry point for the application.
+//
+
+#include "chemical-market-analyzer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
